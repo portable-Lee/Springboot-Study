85752e39ea7026b1a38554222e33f261a68f9077
portable-Lee <qxent300@gmail.com>
2021-03-27 14:06:27 +0900

    JPA - CRUD (CREATE)

---
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class UserRepositoryTest extends StudyApplicationTests {
+
+    // Dependency Injection (DI)
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void create() {
+        // String sql = insert into user (%s, %s, %d) value (account, email, age);
+        User user = new User();
+        user.setAccount("TestUser03");
+        user.setEmail("TestUser03@gmail.com");
+        user.setPhoneNumber("010-3333-3333");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("TestUser3");
+
+        User newUser = userRepository.save(user);
+        System.out.println("newUser : " + newUser);
+    }
+
+    public void read() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void delete() {
+
+    }
+}
