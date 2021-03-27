738081c537fa981eeabdd70f08ec6432dc6e178c
portable-Lee <qxent300@gmail.com>
2021-03-28 02:50:21 +0900

    JPA - CRUD (DELETE)

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -4,9 +4,12 @@ import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
@@ -39,6 +42,7 @@ public class UserRepositoryTest extends StudyApplicationTests {
     }
 
     @Test
+    @Transactional
     public void update() {
         Optional<User> user = userRepository.findById(2L);
 
@@ -51,7 +55,19 @@ public class UserRepositoryTest extends StudyApplicationTests {
         });
     }
 
+    @Test
+    @Transactional
     public void delete() {
+        Optional<User> user = userRepository.findById(3L);
 
+        assertTrue(user.isPresent());    // true
+
+        user.ifPresent(selectUser -> {
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(3L);
+
+        assertFalse(deleteUser.isPresent()); // false
     }
 }
