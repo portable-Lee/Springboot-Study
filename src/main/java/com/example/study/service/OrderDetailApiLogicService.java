cac2603e1d0e62c9625a24444ddd06fd04cd77b2
portable-Lee <qxent300@gmail.com>
2021-04-08 00:06:26 +0900

    OrderDetail API CREATE

---
+++ src/main/java/com/example/study/service/OrderDetailApiLogicService.java
@@ -0,0 +1,74 @@
+package com.example.study.service;
+
+import com.example.study.controller.ifs.CrudInterface;
+import com.example.study.model.entity.OrderDetail;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.OrderDetailApiRequest;
+import com.example.study.model.network.response.OrderDetailApiResponse;
+import com.example.study.repository.ItemRepository;
+import com.example.study.repository.OrderDetailRepository;
+import com.example.study.repository.OrderGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderDetailApiLogicService implements CrudInterface<OrderDetailApiRequest, OrderDetailApiResponse> {
+
+    @Autowired
+    private OrderGroupRepository orderGroupRepository;
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Override
+    public Header<OrderDetailApiResponse> create(Header<OrderDetailApiRequest> request) {
+
+        OrderDetailApiRequest body = request.getData();
+
+        OrderDetail orderDetail = OrderDetail.builder()
+                                             .status(body.getStatus())
+                                             .quantity(body.getQuantity())
+                                             .totalPrice(body.getTotalPrice())
+                                             .orderGroup(orderGroupRepository.getOne(body.getOrderGroupId()))
+                                             .item(itemRepository.getOne(body.getItemId()))
+                                             .build();
+
+        OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
+
+        return response(newOrderDetail);
+    }
+
+    @Override
+    public Header<OrderDetailApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<OrderDetailApiResponse> update(Header<OrderDetailApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<OrderDetailApiResponse> response(OrderDetail orderDetail) {
+
+        OrderDetailApiResponse body = OrderDetailApiResponse.builder()
+                                                            .status(orderDetail.getStatus())
+                                                            .arrivalDate(orderDetail.getArrivalDate())
+                                                            .quantity(orderDetail.getQuantity())
+                                                            .totalPrice(orderDetail.getTotalPrice())
+                                                            .orderGroupId(orderDetail.getOrderGroup().getId())
+                                                            .itemId(orderDetail.getItem().getId())
+                                                            .build();
+
+        return Header.OK(body);
+
+    }
+
+}
