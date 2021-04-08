0ba4f6f7a4bdb5befa631723fc5570303385c5ce
portable-Lee <qxent300@gmail.com>
2021-04-08 23:52:54 +0900

    Partner API CREATE

---
+++ src/main/java/com/example/study/service/PartnerApiLogicService.java
@@ -0,0 +1,80 @@
+package com.example.study.service;
+
+import com.example.study.controller.ifs.CrudInterface;
+import com.example.study.model.entity.Partner;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.PartnerApiRequest;
+import com.example.study.model.network.response.PartnerApiResponse;
+import com.example.study.repository.CategoryRepository;
+import com.example.study.repository.PartnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class PartnerApiLogicService implements CrudInterface<PartnerApiRequest, PartnerApiResponse> {
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public Header<PartnerApiResponse> create(Header<PartnerApiRequest> request) {
+
+        PartnerApiRequest body = request.getData();
+
+        Partner partner = Partner.builder()
+                                 .name(body.getName())
+                                 .status(body.getStatus())
+                                 .address(body.getAddress())
+                                 .callCenter(body.getCallCenter())
+                                 .partnerNumber(body.getPartnerNumber())
+                                 .businessNumber(body.getBusinessNumber())
+                                 .ceoName(body.getCeoName())
+                                 .registeredAt(LocalDateTime.now())
+                                 .category(categoryRepository.getOne(body.getCategoryId()))
+                                 .build();
+
+        Partner newPartner = partnerRepository.save(partner);
+
+        return response(newPartner);
+    }
+
+    @Override
+    public Header<PartnerApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<PartnerApiResponse> update(Header<PartnerApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<PartnerApiResponse> response(Partner partner) {
+
+        PartnerApiResponse body = PartnerApiResponse.builder()
+                                                    .id(partner.getId())
+                                                    .name(partner.getName())
+                                                    .status(partner.getStatus())
+                                                    .address(partner.getAddress())
+                                                    .callCenter(partner.getCallCenter())
+                                                    .partnerNumber(partner.getPartnerNumber())
+                                                    .businessNumber(partner.getBusinessNumber())
+                                                    .ceoName(partner.getCeoName())
+                                                    .registeredAt(partner.getRegisteredAt())
+                                                    .unregisteredAt(partner.getUnregisteredAt())
+                                                    .categoryId(partner.getCategory().getId())
+                                                    .build();
+
+        return Header.OK(body);
+    }
+
+}
